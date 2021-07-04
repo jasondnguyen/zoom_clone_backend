61f2c86318f3e24bcaedf9fd8c5b3ed5941db918
Jason Nguyen <jasondn5@uci.edu>
2021-07-03 19:07:46 -0700

    refactored socket logic

--- server.js
+++ server.js
@@ -17,18 +17,19 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/meeting', require('./routes/api/meeting'));
 
-app.use((req, res, next) => {
-  req.io = io;
-  next();
-});
-
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 io.on('connection', socket => {
-  console.log('socket.io connection made');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId);
+
+  socket.on('newChatMessage', data => {
+    io.in(roomId).emit('newChatMessage', data);
+  });
+
+  socket.on('disconnect', () => {
+    socket.leave(roomId);
   });
 });
